337ef57958c4de0b6b9d9f4586f5a039f4974ad9
Abdul Subhan <abduls.ug22.cs@nitp.ac.in>
2024-12-23 00:05:44 +0300

    initialised vite and react app

---
+++ 01vite/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from "./youtube.jsx"
+function App() {
+
+
+  return (
+    <>
+    <h1>Hello, World!</h1>
+    <Youtube />
+    </>
+  )
+}
+
+export default App
---
+++ 01vite/src/youtube.jsx
@@ -0,0 +1,6 @@
+function Youtube(){
+    return (
+        <h1>YOUTUBE</h1>
+    )
+}
+export default Youtube
